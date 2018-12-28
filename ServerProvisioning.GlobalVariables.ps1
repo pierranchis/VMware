cbf171128f3888e3256f21ea39d15844bdcbb556
pierranchis <44682583+pierranchis@users.noreply.github.com>
2018-12-28 11:31:46 -0500

    Create ServerProvisioning.GlobalVariables.ps1

---
+++ ServerProvisioning.GlobalVariables.ps1
@@ -0,0 +1,16 @@
+$DomainSuffix = 'domain.local'
+$vCenterServer = 'vcenter server'
+$SubnetMask = 'mask'
+$DefaultGateway = 'gateway'
+$DNS01 = 'dns01'
+$DNS02 = 'dns02'
+$OSCusSpec = 'OS Custom Spec Template Name'     
+$Template = 'VM Template Name'
+$ProdVMConvention = "ABCProd*"
+$DevVMConvention = "ABCDev*"
+$ProdCluster = "Production"
+$DevCluster = "Development"
+$ProdFolder = "Prod"
+$DevFolder = "Dev"
+$ProdStorage = 'prodsan*'
+$DevStorage = 'devsan*'
