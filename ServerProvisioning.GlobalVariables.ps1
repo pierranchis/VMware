09a7aa80ab0f4360a8868c5d1ca3af37cb64ef02
pierranchis <44682583+pierranchis@users.noreply.github.com>
2018-12-28 13:17:28 -0500

    Update ServerProvisioning.GlobalVariables.ps1

--- ServerProvisioning.GlobalVariables.ps1
+++ ServerProvisioning.GlobalVariables.ps1
@@ -1,16 +1,31 @@
+# Domain
 $DomainSuffix = 'domain.local'
+
+# vCenter
 $vCenterServer = 'vcenter server'
+
+# vCenter OS Customization Spec Template
 $SubnetMask = 'mask'
 $DefaultGateway = 'gateway'
 $DNS01 = 'dns01'
 $DNS02 = 'dns02'
 $OSCusSpec = 'OS Custom Spec Template Name'     
+
+# vCenter VM Template
 $Template = 'VM Template Name'
+
+# vCenter VM Naming Convention
 $ProdVMConvention = "ABCProd*"
 $DevVMConvention = "ABCDev*"
+
+# vCenter Cluster
 $ProdCluster = "Production"
 $DevCluster = "Development"
+
+#vCenter VM Folders
 $ProdFolder = "Prod"
 $DevFolder = "Dev"
+
+# vCenter Storage
 $ProdStorage = 'prodsan*'
 $DevStorage = 'devsan*'
